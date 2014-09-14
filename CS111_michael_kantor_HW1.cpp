bc0d76762d7dbd1ba89eec9bef0cbc16af6c35d1
mikenolimits <mkantor100@qc.cuny.edu>
2014-09-14 11:53:19 -0400

    tidied up

--- CS111_michael_kantor_HW1.cpp
+++ CS111_michael_kantor_HW1.cpp
@@ -5,9 +5,11 @@ int main(){
 
 unsigned int width;
 unsigned int height;
-//floating points in most languages will always give the same value, but it will be rounded off to an aproximate integer. This is due to the fact that more integers = more bbytes taken up & more inefficiency. Floating points remedy the preformance hit in exchange for less accurate calculations. 
+// Floating points in most languages will always give the same value, but it will be rounded off to an aproximate integer.
+// This is due to the fact that more integers = more bbytes taken up & more inefficiency.
+// Floating points remedy the preformance hit in exchange for less accurate calculations. 
 
-cout<<"Hi. My name is Steve, the rectangle calculater. Please note that I calculate longer integers as floating points, due to this, larger calculations might not appear to be as accurate."; 
+cout<<"Hi. My name is Steve, the rectangle calculater."<<"\n"<<"Please note that I calculate longer integers as floating points, due to this, larger calculations might not appear to be as accurate."<<"\n"; 
 
 cout<<"Now that we cleared that up....What is the width of your rectangle?";
 
