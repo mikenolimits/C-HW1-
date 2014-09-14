6d67d91264ce0f1e4e2a9fa05a8dc213b447bd34
Michael Kantor <kami8156@venus.cs.qc.cuny.edu>
2014-09-14 11:34:49 -0400

    first commit

---
+++ CS111_michael_kantor_HW1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+unsigned int width;
+unsigned int height;
+//floating points in most languages will always give the same value, but it will be rounded off to an aproximate integer. This is due to the fact that more integers = more bbytes taken up & more inefficiency. Floating points remedy the preformance hit in exchange for less accurate calculations. 
+
+cout<<"Hi. My name is Steve, the rectangle calculater. Please note that I calculate longer integers as floating points, due to this, larger calculations might not appear to be as accurate."; 
+
+cout<<"Now that we cleared that up....What is the width of your rectangle?";
+
+cin>>width;
+
+cout<<"Thanks! What is the height of your rectangle?";
+
+cin>>height;
+
+cout<<"Great. The area of your rectangle is aproximately equal to " << width * height <<"\n";
+return 0;
+
+}
